1e6b1790cc3c5dc28444c8b7f4751113455af106
francis556 <vikrammehra244@gmail.com>
2022-04-19 07:06:20 +0000

    initial setup and configuration

--- Coming_soon/settings.py
+++ Coming_soon/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y+xzcn=848&50%i@4gt(5gutq%q&qq9=g%6-5b)a!j5!ttc&=n'
+SECRET_KEY = 'y+xz5586558gffn=848&50%i@4gt(5gutq%q&qq9=g%6-5b)a!j5!ttc&=n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
