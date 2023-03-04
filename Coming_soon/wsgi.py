7d065e74b7a07b24ba7bccb2756c63eaddacd169
francis556 <vikrammehra244@gmail.com>
2023-03-04 17:38:44 +0200

    info: separate project env files

--- Coming_soon/wsgi.py
+++ Coming_soon/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for Coming_soon project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coming_soon.settings')
 
+dotenv.load_dotenv(
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coming_soon.dev')
+
+if os.getenv('DJANGO_SETTINGS_MODULE'):
+    os.environ['DJANGO_SETTINGS_MODULE'] = os.getenv('DJANGO_SETTINGS_MODULE')
+    project_environment = os.environ['DJANGO_SETTINGS_MODULE']
+
+env = project_environment
 application = get_wsgi_application()
